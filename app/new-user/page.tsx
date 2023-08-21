ac4eabdf9095e9db8643e2ad30dec41b3284644f
dillonzellis <dillonzellis@gmail.com>
2023-08-21 17:03:41 -0400

    new user page

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,7 +1,35 @@
-const NewUser = () => {
-  return (
-    <div>yoo</div>
-  )
+import { prisma } from "@/util/db";
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
+
+const createNewUser = async () => {
+
+  const user = await currentUser()
+
+  // console.log(user);
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string
+    }
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      }
+    })
+  }
+
+  redirect('/journal')
+
 }
 
-export default NewUser
+const NewUser = async () => {
+  await createNewUser()
+  return <div>...loading</div>
+}
+
+export default NewUser;
